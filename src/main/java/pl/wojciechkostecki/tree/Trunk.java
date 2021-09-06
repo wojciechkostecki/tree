b78729c23309fb8eea38c79e71d0b5559da0d50c
48531 <wt.kostecki@gmail.com>
2021-09-06 12:42:20 +0200

    Create Trunk class

---
+++ src/main/java/pl/wojciechkostecki/tree/Trunk.java
@@ -0,0 +1,23 @@
+package pl.wojciechkostecki.tree;
+
+public class Trunk {
+    private double widthTrunk;
+
+    private double heightTrunk;
+
+    public double getWidthTrunk() {
+        return widthTrunk;
+    }
+
+    public void setWidthTrunk(double widthTrunk) {
+        this.widthTrunk = widthTrunk;
+    }
+
+    public double getHeightTrunk() {
+        return heightTrunk;
+    }
+
+    public void setHeightTrunk(double heightTrunk) {
+        this.heightTrunk = heightTrunk;
+    }
+}
